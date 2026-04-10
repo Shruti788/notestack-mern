73e1a8abb5eb53ecede8cff5284cc7de0379c55b
Shruti Yadav <shrutiycs78@gmail.com>
2026-04-10 22:19:20 +0530

    fix deployment case issue

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -4,9 +4,8 @@ import dotenv from "dotenv"
 import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
-import {connectDB} from "./config/db.js"
+import { connectDB } from "./config/db.js"
 import rateLimiter from "./middleware/rateLimiter.js"
-
 
 dotenv.config()
 
@@ -15,39 +14,28 @@ const PORT = process.env.PORT || 5001
 const __dirname = path.resolve()
 
 // middleware
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
+app.use(cors({
+  origin: "*"
+}))
 
 app.use(express.json())
 app.use(rateLimiter)
 
-
-// app.use((req, res, next) => {
-//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`)
-//     next()
-// })
-
 app.use("/api/notes", notesRoutes)
 
+// ✅ Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../FRONTEND/dist")))
 
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONTEND/dist/index.html"))
+  })
 }
 
-
 connectDB().then(() => {
-    app.listen(PORT, () => {
-    console.log("Server is running on PORT:", PORT)
-})
-
+  app.listen(PORT, () => {
+    console.log("Server running on PORT:", PORT)
+  })
 })
 
 
